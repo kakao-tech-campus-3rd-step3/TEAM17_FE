abc0e20da30c86fec3335b9c54d132b6ed0e4c61
ppre1ude <cjh5110@naver.com>
2025-09-12 01:20:47 +0900

    feat: Best 스타터팩 부분 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from '@/pages/banner/Banner';
 import Category from '@/pages/category/Category';
+import StarterPreview from '@/components/starterpreview/StarterPreview';
 
 const HomePage = () => {
   return (
@@ -8,6 +9,7 @@ const HomePage = () => {
       <main className="p-4 space-y-3">
         <Banner />
         <Category />
+        <StarterPreview />
       </main>
     </div>
   );
