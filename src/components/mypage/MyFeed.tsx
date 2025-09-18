3b024eae2e78fcfa59e605eaa922ffd171d6bfb3
sumin <ssumin512@gmail.com>
2025-09-19 03:33:13 +0900

    feat: 마이페이지 업로드한 피드 컴포넌트 50% 구현

---
+++ src/components/mypage/MyFeed.tsx
@@ -0,0 +1,38 @@
+import {
+  Container,
+  FeedList,
+  FeedContainer,
+  TopContainer,
+  ProfileImage,
+  InfoContainer,
+  Nickname,
+  SubInfo,
+  Line,
+  Image,
+} from '@/components/mypage/MyFeed.style';
+import homeBaking from '@/assets/homeBaking.jpg';
+import example from '@/assets/example.jpg';
+
+const MyFeed = () => {
+  return (
+    <Container>
+      <FeedList>
+        {[1, 2, 3, 4].map((item) => (
+          <FeedContainer key={item}>
+            <TopContainer>
+              <ProfileImage src={homeBaking} />
+              <InfoContainer>
+                <Nickname>빵수니</Nickname>
+                <SubInfo>INFP 감성 빵 제조기입니당~</SubInfo>
+              </InfoContainer>
+            </TopContainer>
+            <Line />
+            <Image src={example} />
+          </FeedContainer>
+        ))}
+      </FeedList>
+    </Container>
+  );
+};
+
+export default MyFeed;
--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,9 +1,10 @@
 import Profile from '@/components/mypage/Profile';
-
+import MyFeed from '@/components/mypage/MyFeed';
 const Mypage = () => {
   return (
     <>
       <Profile></Profile>
+      <MyFeed></MyFeed>
     </>
   );
 };
