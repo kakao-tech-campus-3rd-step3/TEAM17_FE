87d9a84ac0f4018fcb7cd4b436587243749e9f7c
sumin <ssumin512@gmail.com>
2025-09-25 21:33:39 +0900

    작업 중인 내용 커밋

--- src/components/feedwriting/ThumbnailImage.tsx
+++ src/components/feedwriting/ThumbnailImage.tsx
@@ -1,11 +1,18 @@
+//import {useRef} from 'react';
 import { Desc, TitleStyle } from '@/components/feedwriting/Title.style';
 import { ImageUploadBox } from '@/components/feedwriting/UploadBox.style';
 
 const ThumbnailImage = () => {
+
+  //const fileInputRef = useRef<HTMLInputElement|null>(null);
+
+  const handleBoxClick = () =>{
+    console.log('boxclick')
+  }
   return (
     <>
       <TitleStyle>대표 이미지 업로드</TitleStyle>
-      <ImageUploadBox>
+      <ImageUploadBox onClick={handleBoxClick}>
         <Desc>
           대표 이미지를 업로드해주세요. <br /> 클릭 시 이미지 불러오기
         </Desc>
