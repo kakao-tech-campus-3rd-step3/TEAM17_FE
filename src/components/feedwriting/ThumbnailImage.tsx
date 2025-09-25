893f8deb21cae9ae4719b6a5ce3673822e04f995
sumin <ssumin512@gmail.com>
2025-09-25 22:14:55 +0900

    feat: 이미지업로드 박스 클릭 파일 선택창 오픈

--- src/components/feedwriting/ThumbnailImage.tsx
+++ src/components/feedwriting/ThumbnailImage.tsx
@@ -1,14 +1,14 @@
-//import {useRef} from 'react';
+import { useRef } from 'react';
 import { Desc, TitleStyle } from '@/components/feedwriting/Title.style';
 import { ImageUploadBox } from '@/components/feedwriting/UploadBox.style';
 
 const ThumbnailImage = () => {
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
 
-  //const fileInputRef = useRef<HTMLInputElement|null>(null);
+  const handleBoxClick = () => {
+    fileInputRef.current?.click();
+  };
 
-  const handleBoxClick = () =>{
-    console.log('boxclick')
-  }
   return (
     <>
       <TitleStyle>대표 이미지 업로드</TitleStyle>
@@ -16,6 +16,7 @@ const ThumbnailImage = () => {
         <Desc>
           대표 이미지를 업로드해주세요. <br /> 클릭 시 이미지 불러오기
         </Desc>
+        <input ref={fileInputRef} type="file" accept="image/*" style={{ display: 'none' }} />
       </ImageUploadBox>
     </>
   );
