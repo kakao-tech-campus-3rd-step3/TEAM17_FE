55a258a41de37f3eb9f46b3c29141224721e0c1b
sumin <ssumin512@gmail.com>
2025-10-02 02:18:55 +0900

    refactor: 로그인/회원가입 hook을 reactQuery 기반 리팩토링

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,6 +1,35 @@
 import { useContext } from 'react';
 import { AuthContext } from '@/contexts/AuthContext';
 
+import { useMutation } from '@tanstack/react-query'
+import { login, signup } from '@/api/auth'
+import { QUERY_KEYS } from '@/utils/queryKeys'
+
+import type { LoginValues } from '@/types/LoginZodSchema'
+import type { Step2Values } from '@/types/SignupZodSchema'
+
+export const useLogin = () => {
+  return useMutation({
+    mutationKey: QUERY_KEYS.login,
+    mutationFn: (data: LoginValues) => login(data),
+    onSuccess: (data) => {
+      if (data.accessToken) {
+        localStorage.setItem('accessToken', data.accessToken)
+      }
+    },
+  })
+}
+
+export const useSignup = () => {
+  return useMutation({
+    mutationKey: QUERY_KEYS.signup,
+    mutationFn: (data: Step2Values) => signup(data),
+    onSuccess: () => {
+      console.log('✅ 회원가입 성공')
+    },
+  })
+}
+
 export function useAuth() {
   const context = useContext(AuthContext);
   if (!context) {
