e74743c3aaf58f5c3a832a2fd820643639c7413f
sumin <ssumin512@gmail.com>
2025-10-10 16:16:38 +0900

    chore: https 설정 후 .gitignore에 cert폴더 관련 정보 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import fs from 'fs';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +11,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-});
+  server: {
+    https: {
+      key: fs.readFileSync('./cert/localhost-key.pem'),
+      cert: fs.readFileSync('./cert/localhost-cert.pem'),
+    },
+    port: 5173,
+  },
+})
