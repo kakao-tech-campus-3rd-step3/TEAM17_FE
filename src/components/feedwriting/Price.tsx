71f6f68c1fd4d9b7c720976657be116dbc906a1f
sumin <ssumin512@gmail.com>
2025-10-13 13:24:12 +0900

    feat: 가격 입력 받기 및 가격 입력 유효성 추가

--- src/components/feedwriting/Price.tsx
+++ src/components/feedwriting/Price.tsx
@@ -1,15 +1,41 @@
+import { useState } from 'react';
 import { Desc, TitleStyle } from '@/components/feedwriting/Title.style';
 import { PriceContainer, PriceBox } from '@/components/feedwriting/UploadBox.style';
+import { formatNumberInput, parseNumberInput } from '@/utils/price';
+
 const Price = () => {
+  const [price, setPrice] = useState<number>(0);
+  const [displayValue, setDisplayValue] = useState<string>('0');
+  const [isActive, setIsActive] = useState(false);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const rawInput = e.target.value;
+
+    const formatted = formatNumberInput(rawInput);
+    setDisplayValue(formatted);
+
+    const numeric = parseNumberInput(formatted);
+    setPrice(numeric);
+  };
+
   return (
-    <>
-      <PriceContainer>
-        <TitleStyle>가격대 (전체적)</TitleStyle>
-        <PriceBox>
+    <PriceContainer>
+      <TitleStyle>가격대 (전체적)</TitleStyle>
+      <PriceBox onClick={() => setIsActive(true)}>
+        {isActive ? (
+          <input
+            type="text"
+            inputMode="numeric"
+            value={displayValue}
+            onChange={handleChange}
+            required
+            style={{ border: 'none', outline: 'none' }}
+          />
+        ) : (
           <Desc>대략적인 가격을 작성해주세요</Desc>
-        </PriceBox>
-      </PriceContainer>
-    </>
+        )}
+      </PriceBox>
+    </PriceContainer>
   );
 };
 
