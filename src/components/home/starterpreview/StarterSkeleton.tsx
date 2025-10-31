370230d98db6b78585b585729346548e84319773
ppre1ude <cjh5110@naver.com>
2025-10-31 14:05:21 +0900

    style: 스켈레톤 애니메이션 스타일 요소 점검

--- src/components/home/starterpreview/StarterSkeleton.tsx
+++ src/components/home/starterpreview/StarterSkeleton.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import Skeleton from '@/components/common/Skeleton';
 import { GridItem } from './StarterPreview.styles';
+import styled from 'styled-components';
+
+const SkeletonWrapper = styled(GridItem)`
+  position: relative;
+  background-color: transparent;
+  overflow: hidden;
+  display: block;
+
+  > * {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+`;
 
 const StarterSkeleton: React.FC = () => {
   return (
-    <GridItem>
-      <Skeleton height="100%" borderRadius="0.5rem" />
-    </GridItem>
+    <SkeletonWrapper>
+      <Skeleton width="100%" height="100%" borderRadius="0.5rem" />
+    </SkeletonWrapper>
   );
 };
 
