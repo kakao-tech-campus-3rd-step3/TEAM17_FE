ef699040adf06ebc58e2256cf229dde56bc1ee5b
ppre1ude <cjh5110@naver.com>
2025-11-04 18:42:59 +0900

    fix: dirName prop 타입 오류 해결

--- src/hooks/useUploadImages.ts
+++ src/hooks/useUploadImages.ts
@@ -2,14 +2,10 @@ import { useMutation } from '@tanstack/react-query';
 import { getPresignedUrls, uploadToS3 } from '@/api/s3Api';
 import { AxiosError } from 'axios';
 
-export const useUploadImages = () => {
-  return useMutation<
-    string[], 
-    AxiosError<{ message?: string }>,
-    File[] 
-  >({
+export const useUploadImages = (dirName: string = 'feed') => {
+  return useMutation<string[], AxiosError<{ message?: string }>, File[]>({
     mutationFn: async (files: File[]) => {
-      const presignedList = await getPresignedUrls('feed', files);
+      const presignedList = await getPresignedUrls(dirName, files);
 
       await Promise.all(
         presignedList.map((item, idx) => uploadToS3(item.presignedUrl, files[idx]))
